e1b7cf20a5d2641a70fcebfd43064039fce4d345
MARKMENTAL <marky611@gmail.com>
2022-01-16 12:40:45 -0500

    Added FreeBSD, code cleanup

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # DistroDownloader
 DistroDownloader is a simple program that is extremely useful for users who often switch between different Linux distributions.
 Credit is given to Aaron-JM for creating the original Python version, This new version is updated for 2021 and written in C.
-The number of distros included was reduced to 5 GNU/Linux distros from the original's 15. I chose distros with complete installs and a good amount of support.
+The number of distros included was reduced to 7 Linux and Unix distros from the original's 15. I chose distros with complete installs and a good amount of support.
 
 # System Requirements
 This program is written as a C interface for a Linux shell that includes an installation of wget. So make sure that you are running Linux or WSL
--- distrodownloader.c
+++ distrodownloader.c
@@ -10,62 +10,63 @@ void distrosites(){
     printf("https://getfedora.org\n");
     printf("https://neon.kde.org\n");
     printf("https://www.endeavouros.com\n");
-    printf("https://www.manjaro.org\n\n");
+    printf("https://www.manjaro.org\n");
+    printf("https://www.freebsd.org\n");
 }
 
 void exitfunc(){
-	printf("Program will now close\n");
+    printf("Program will now close\n");
 }
 
 
-void completemsg(){
+void completemsg(char directorysave[50]){
     distrosites();
-    printf("Download Process Complete\nCheck the folder you ran the program from for your downloaded '.iso'\n");
+    printf("Download Process Complete\nCheck %s  for your downloaded '.iso'\n", directorysave);
 }
 
 
 void manjaro(char directorysave[50]){
-	unsigned int manjaro_response;
-	printf("\nChoose a version to install...\n\n");
-	printf("0. Exit\n");
-	printf("1. XFCE Edition\n");
-	printf("2. KDE Plasma Edition\n");
-	printf("3. Gnome Edition\n\n");
-	printf("Select Version Number: ");
-	scanf("%i", &manjaro_response);
+    unsigned int manjaro_response;
+    printf("\nChoose a version to install...\n\n");
+    printf("0. Exit\n");
+    printf("1. XFCE Edition\n");
+    printf("2. KDE Plasma Edition\n");
+    printf("3. Gnome Edition\n\n");
+    printf("Select Version Number: ");
+    scanf("%i", &manjaro_response);
 
-	if (manjaro_response == 0){
-		exitfunc();	
-	}
+    if (manjaro_response == 0){
+        exitfunc();    
+    }
 
-	else if (manjaro_response == 1){
-		char wgetcmd[200] = "wget https://download.manjaro.org/xfce/21.2rc1/manjaro-xfce-21.2rc1-211210-linux515.iso -P ";
-		strcat(wgetcmd, directorysave);
-		system(wgetcmd);
-		completemsg();	
-	}
+    else if (manjaro_response == 1){
+        char wgetcmd[200] = "wget https://download.manjaro.org/xfce/21.2rc1/manjaro-xfce-21.2rc1-211210-linux515.iso -P ";
+        strcat(wgetcmd, directorysave);
+        system(wgetcmd);
+        completemsg(directorysave);    
+    }
 
-	else if (manjaro_response == 2){
-		char wgetcmd[200] = "wget https://download.manjaro.org/kde/21.2rc1/manjaro-kde-21.2rc1-211211-linux515.iso -P ";
-		strcat(wgetcmd, directorysave);
-                system(wgetcmd);
-		completemsg();
-	}
+    else if (manjaro_response == 2){
+        char wgetcmd[200] = "wget https://download.manjaro.org/kde/21.2rc1/manjaro-kde-21.2rc1-211211-linux515.iso -P ";
+        strcat(wgetcmd, directorysave);
+        system(wgetcmd);
+        completemsg(directorysave);
+    }
 
-	else if (manjaro_response == 3){
-		char wgetcmd[200] = "wget https://download.manjaro.org/gnome/21.2rc1/manjaro-gnome-21.2rc1-211211-linux515.iso -P ";
-		strcat(wgetcmd, directorysave);
-                system(wgetcmd);
-		completemsg();
-	}
-	
+    else if (manjaro_response == 3){
+        char wgetcmd[200] = "wget https://download.manjaro.org/gnome/21.2rc1/manjaro-gnome-21.2rc1-211211-linux515.iso -P ";
+        strcat(wgetcmd, directorysave);
+        system(wgetcmd);
+        completemsg(directorysave);
+    }
+
 }
 
 
 int main(){
     unsigned int response;
     char directorysave[50];
-    printf("Select a GNU/Linux Distribution...\n");
+    printf("Select a Linux or Unix Distribution...\n");
     printf("0. Exit\n");
     printf("1. Ubuntu 21.10 - 64bit\n");
     printf("2. Debian 11 NetInstall - 64bit\n");
@@ -73,6 +74,7 @@ int main(){
     printf("4. KDE Neon - 64bit\n");
     printf("5. EndeavourOS - 64bit\n");
     printf("6. Manjaro - 64bit\n");
+    printf("7. FreeBSD 13 - 64bit\n");
     printf("Select Distribution Number: ");
     scanf("%i", &response);
 
@@ -82,45 +84,53 @@ int main(){
 
     if (response == 1){
         char wgetcmd[200] = "wget https://releases.ubuntu.com/21.10/ubuntu-21.10-desktop-amd64.iso -P ";
-	strcat(wgetcmd, directorysave);
-	system(wgetcmd);
-        completemsg();
+        strcat(wgetcmd, directorysave);
+        system(wgetcmd);
+        completemsg(directorysave);
     }
 
     else if (response == 2){
         char wgetcmd[200] = "wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.2.0-amd64-netinst.iso -P ";
-	strcat(wgetcmd, directorysave);
+        strcat(wgetcmd, directorysave);
         system(wgetcmd);
-        completemsg();
+        completemsg(directorysave);
     }
 
     else if (response == 3){
         char wgetcmd[200] = "wget https://download.fedoraproject.org/pub/fedora/linux/releases/35/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-35-1.2.iso -P ";
-	strcat(wgetcmd, directorysave);
+        strcat(wgetcmd, directorysave);
         system(wgetcmd);
-        completemsg();
+        completemsg(directorysave);
     }
 
     else if (response == 4){
         char wgetcmd[200] = "wget https://files.kde.org/neon/images/user/20211216-0945/neon-user-20211216-0945.iso -P ";
-	strcat(wgetcmd, directorysave);
+        strcat(wgetcmd, directorysave);
         system(wgetcmd);
-        completemsg();
+        completemsg(directorysave);
     }
 
     else if (response == 5){
-    	char wgetcmd[200] = "wget https://github.com/endeavouros-team/ISO/releases/download/1-EndeavourOS-ISO-releases-archive/EndeavourOS_Atlantis_neo-21_5.iso -P ";
-	strcat(wgetcmd, directorysave);
+        char wgetcmd[200] = "wget https://github.com/endeavouros-team/ISO/releases/download/1-EndeavourOS-ISO-releases-archive/EndeavourOS_Atlantis_neo-21_5.iso -P ";
+        strcat(wgetcmd, directorysave);
         system(wgetcmd);
-    	completemsg();
+        completemsg(directorysave);
     }
 
     else if (response == 6){
-    	manjaro(directorysave);
+        manjaro(directorysave);
+    }
+
+    else if (response == 7){
+        char wgetcmd[200] = "wget https://download.freebsd.org/releases/amd64/amd64/ISO-IMAGES/13.0/FreeBSD-13.0-RELEASE-amd64-disc1.iso -P ";
+        strcat(wgetcmd, directorysave);
+        system(wgetcmd);
+        completemsg(directorysave);
     }
 
     else{
-	exitfunc();
+        exitfunc();
     }
+
     return 0;
 }
